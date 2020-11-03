401dcbe053e038b4f074bcd710b404183c7b5fd0
John A Maloney <john.a.maloney@gmail.com>
2020-11-03 15:06:56 -0600

    Reset checksums, readd lodash

--- ecom-client/src/components/LoginRegister.js
+++ ecom-client/src/components/LoginRegister.js
@@ -1,19 +1,19 @@
-import React, { useContext } from "react";
-import _ from 'lodash';
-import { useHistory, useLocation, useRouteMatch } from "react-router-dom";
-import { Form, FormField, TextInput, Button } from "grommet";
-import { Context as AuthContext } from "../context/AuthContext";
-import Layout from "./layout";
-import useForm from "../hooks/useForm";
-import validate from "../lib/formValidate";
+import React, { useContext } from 'react';
+import compact from 'lodash/compact';
+import { useHistory, useLocation, useRouteMatch } from 'react-router-dom';
+import { Form, FormField, TextInput, Button } from 'grommet';
+import { Context as AuthContext } from '../context/AuthContext';
+import Layout from './layout';
+import useForm from '../hooks/useForm';
+import validate from '../lib/formValidate';
 
 const LoginRegister = () => {
   const { url } = useRouteMatch();
-  const pageType = _.compact(url.split('/'))[0]
+  const pageType = compact(url.split('/'))[0];
   const {
     doLogin,
     doRegister,
-    state: { isAuthenticated, fromSession }
+    state: { isAuthenticated, fromSession },
   } = useContext(AuthContext);
   const handleFormSubmit = pageType === 'register' ? doRegister : doLogin;
   const { values, errors, handleChange, handleSubmit } = useForm(
@@ -25,18 +25,21 @@ const LoginRegister = () => {
   const location = useLocation();
 
   const redirect = (timeout = 0) => {
-    const { from } = location.state || { from: { pathname: "/" } };
+    const { from } = location.state || { from: { pathname: '/' } };
     setTimeout(() => history.replace(from), timeout);
   };
 
   return (
     <Layout>
       {isAuthenticated ? (
-        fromSession ? redirect() :
-        <div>
-          <h1>Success, you're logged in! Redirecting you...</h1>
-          {redirect(1500)}
-        </div>
+        fromSession ? (
+          redirect()
+        ) : (
+          <div>
+            <h1>Success, you're logged in! Redirecting you...</h1>
+            {redirect(1500)}
+          </div>
+        )
       ) : (
         <Form onSubmit={handleSubmit}>
           <TextInput
